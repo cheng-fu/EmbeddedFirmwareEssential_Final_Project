d4a7f4c240333b8179d427bbf44e1f7800a291b2
chengfu16 <chengfu16@gmail.com>
2018-10-27 03:53:21 -0700

    add power management feature

---
+++ Final_Project/SystemPowerManagement.h
@@ -0,0 +1,60 @@
+/*
+ * SystemPower_Management.h
+ *
+ *  Created on: Oct 26, 2018
+ *      Author: fu
+ */
+
+#ifndef SYSTEMPOWERMANAGEMENT_H_
+#define SYSTEMPOWERMANAGEMENT_H_
+
+#include <stdint.h>
+#include "mbed_sleep.h"
+#include "Serial_pc.h"
+
+#define SYSTEM_CONTROL_REG (*(volatile uint32_t *) (0xE000ED10))
+#define INTTERUPT_SET_ENABLE_REG (* (volatile uint32_t *) (0xE000E100))
+#define INTTERUPT_SET_ENABLE_REG_STOP (* (volatile uint32_t *) (0xE000E11C))
+#define INTTERUPT_CLEAR_ENABLE_REG (* (volatile uint32_t *) (0xE000E180))
+#define INTTERUPT_CLEAR_ENABLE_REG_STOP (* (volatile uint32_t *) (0xE000E18C))
+
+#define SYSTEM_SLEEP_DEBUG_PRINT true
+
+volatile uint32_t sleep_cnt;
+volatile int is_asleep = 0;
+volatile unsigned int last_sleep_duration = 0;
+volatile unsigned long total_sleep_time = 0;
+
+// System power indicator
+DigitalOut power_led(p28);
+
+void system_sleep(){
+
+	is_asleep = 1;
+	power_led = 0;
+	// Put system to deep sleep, set SLEEPONEXIT bit
+	SYSTEM_CONTROL_REG |= 0x0002;
+	int sleep_starts_at = global_tm.read_ms();
+	sleep();
+	sleep_cnt += 1;
+	last_sleep_duration = global_tm.read_ms() - sleep_starts_at;
+	total_sleep_time += last_sleep_duration;
+};
+
+bool is_system_asleep() {
+	//uint32_t ret = SYSTEM_CONTROL_REG & 0x0010;
+	//return ret == 0;
+	return is_asleep == 1;
+};
+
+void system_wakeup() {
+	// Exit from sleep mode, runs in interrupt context
+	// Clear SLEEPONEXIT bit
+	SYSTEM_CONTROL_REG &= (~0x0002);
+	is_asleep = 0;
+	power_led = 1;
+}
+
+
+
+#endif /* SYSTEMPOWERMANAGEMENT_H_ */
--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "mbed.h"
+
 #include "Temp102.h"
 #include "TextLCD.h"
 #include "Accel_ADXL345_I2C.h"
@@ -12,16 +13,25 @@
 #include "Serial_pc.h"
 #include "Display_Brightness_control.h"
 #include "Button_Interrupt.h"
+#include "SystemPowerManagement.h"
 
 TextLCD lcd(p19, p20, p24, p23, p22, p21); // rs, e, d4-d7
 
-#define MAIN_LOG_ENABLE false
+#define MAIN_LOG_ENABLE true
+
+// SET to true if print on every loop, false every 10 loops
+#define MAIN_VERBOSe_LOG false
 
 ALS_TSL2561 als_sensor;
 Accel_ADXL345 accel_sensor;
 TMP_102 temp_sensor;
 Display_Brightness_Controller display_brightness;
 Button button;
+
+int16_t accel_data[3];
+float temp = 0.0;
+float x, y, z;
+volatile unint idle_loop_count = 0;
 
 void DisplayOff_Callback(){
 	if(display_state == 1) {
@@ -33,25 +43,28 @@ void DisplayOff_Callback(){
 }
 
 void reschedule_DisplayOff_TO(float TO) {
+  if(is_system_asleep()) {
+	  system_wakeup();
+	  device_was_facing_down = 0;
+  }
   DisplayOff_to.detach();
   if(display_state == 0) {
 	  display_brightness.turn_on_display();
 	  als_sensor.start();
 	  temp_sensor.start();
+	  idle_loop_count = 0;
   }
-
   display_state = 1;
   DisplayOff_to.attach(&DisplayOff_Callback, TO);
 }
 
-int16_t accel_data[3];
-float temp = 0.0;
-float x, y, z;
 
 int main()
 {
 	unint als_data[2] = {0,0};
 	pc.baud(115200);
+	// initialize system power LED to 1
+	power_led = 1;
 	pc.printf("\r\n=====================\r\n");
 	pc.printf("System starting...\r\n");
 	global_tm.start();
@@ -61,6 +74,18 @@ int main()
 	DisplayOff_to.attach(&DisplayOff_Callback, default_DisplayOff_TO);
 	while(1)
 	{
+		if (idle_loop_count >= 62) {
+#if MAIN_LOG_ENABLE
+			pc.printf("Device going to sleep ... \r\n");
+#endif
+			system_sleep();
+#if MAIN_LOG_ENABLE
+			pc.printf("Device waken up from sleep ... \r\n");
+			wait_ms(100);
+#endif
+			continue;
+		}
+
 		wait_ms(200);
 		// Read Accel data
 		accel_sensor.read_accel_data(accel_data);
@@ -80,31 +105,47 @@ int main()
 			}
 			device_face_down = 0;
 		}
-
+		bool new_samples_read = false;
 		// Check display state and print to Text LCD
 		if (display_state) {
+			idle_loop_count = 0;
 			float temp_data = temp_sensor.read_data();
 			if (temp_data > - 50.0) temp = temp_data;
 			als_sensor.read_data(als_data);
+			new_samples_read = true;
 			display_brightness.adjust_brightness(als_data[0]);
 			lcd.cls();
 			lcd.locate(0,0);
 			lcd.printf("T %.2f,x %+4.2f,y %+4.2f,z %+4.2f",temp, x, y, z);
+		} else {
+			idle_loop_count += 1;
 		}
 		wait_ms(200);
 
 #if MAIN_LOG_ENABLE
-		pc.printf("Temp = %.3f deg C, x:  %+4.3f g, y:  %+4.3f g, z:  %+4.3f g\r\n",temp, x, y, z);
-		pc.printf("accel int_cnt = %u, button int_cnt = %u,\r\n", accel_int_cnt, but_int_cnt);
-		if (display_state) {
-			pc.printf("ALS ch0: %u, ch1: %u", als_data[0], als_data[1]);
-			pc.printf(", Current brightness %.3f\r\n", display_brightness.get_brightness());
+#if not MAIN_VERBOSE_LOG
+		static unint loop_count = 0;
+		if (loop_count >= 8) {
+			loop_count = 0;
+#endif
+			pc.printf("Accel x:  %+4.3f g, y:  %+4.3f g, z:  %+4.3f g\r\n", x, y, z);
+			if (new_samples_read) {
+				pc.printf("Temp = %.3f deg C, ",temp);
+				pc.printf("ALS ch0: %u ch1: %u", als_data[0], als_data[1]);
+				pc.printf(", Display brightness %.3f\r\n", display_brightness.get_brightness());
+			}
+			pc.printf("accel int_cnt = %u, button int_cnt = %u, sleep_cnt = %lu\r\n", accel_int_cnt, but_int_cnt, sleep_cnt);
+			pc.printf("last_sleep_duration = %d ms, total_sleep_time = %lu ms, system_up_time = %d ms\r\n", last_sleep_duration, total_sleep_time, global_tm.read_ms());
+			if (device_face_down) {
+				pc.printf("Device facing down\r\n");
+			} else if (device_was_facing_down) {
+				pc.printf("Device was facing down\r\n");
+			}
+#if not MAIN_VERBOSE_LOG
+		} else {
+			loop_count += 1;
 		}
-		if (device_face_down) {
-			pc.printf("Device facing down\r\n");
-		} else if (device_was_facing_down) {
-			pc.printf("Device was facing down\r\n");
-		}
+#endif
 #endif
 
 	}
